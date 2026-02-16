1cf14676e441d4c1344de55ad3c891d5a45bbd7c
FilipPopescu <filippopescu18@gmail.com>
2026-02-16 19:41:23 +0200

    Points get drawn better

--- GraphEditor/Views/MainView.xaml.cs
+++ GraphEditor/Views/MainView.xaml.cs
@@ -1,10 +1,7 @@
-﻿using GraphEditor.Models;
-using GraphEditor.ViewModels;
+﻿using GraphEditor.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Shapes;
 
 namespace GraphEditor.Views;
 
@@ -13,8 +10,6 @@ namespace GraphEditor.Views;
 /// </summary>
 public partial class MainView : UserControl
 {
-    private const double NodeSize = 25;
-
     public MainView()
     {
         InitializeComponent();
@@ -22,45 +17,9 @@ public partial class MainView : UserControl
 
     private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
-        var vm = DataContext as MainViewModel;
+        var vm = (DataContext as MainViewModel)!;
         var clickPosition = e.GetPosition(sender as UIElement);
-        vm?.AddNodeCommand.Execute(clickPosition);
-        DrawPoints(vm!.Nodes);
-    }
-
-    private void DrawPoints(IEnumerable<Node> nodes)
-    {
-        foreach (var node in nodes)
-        {
-            var ellipse = new Ellipse
-            {
-                Width = NodeSize,
-                Height = NodeSize,
-                Fill = Brushes.White,
-                Stroke = Brushes.Black,
-                StrokeThickness = 1
-            };
-
-            var text = new TextBlock
-            {
-                Text = node.Number.ToString(),
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-                FontWeight = FontWeights.Bold
-            };
-
-            var nodeGrid = new Grid
-            {
-                Width = NodeSize,
-                Height = NodeSize
-            };
-            nodeGrid.Children.Add(ellipse);
-            nodeGrid.Children.Add(text);
-
-            Canvas.SetLeft(nodeGrid, node.Position.X - NodeSize / 2);
-            Canvas.SetTop(nodeGrid, node.Position.Y - NodeSize / 2);
-
-            PointsCanvas.Children.Add(nodeGrid);
-        }
+        vm.AddNodeCommand.Execute(clickPosition);
+        vm.UpdateCanvas(PointsCanvas);
     }
 }
