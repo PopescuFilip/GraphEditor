0166b3001734e311e98f3d90ae0c3e969fb26c16
FilipPopescu <filippopescu18@gmail.com>
2026-02-16 19:30:17 +0200

    Drawing points works

--- GraphEditor/ViewModels/MainViewModel.cs
+++ GraphEditor/ViewModels/MainViewModel.cs
@@ -2,19 +2,18 @@
 using GraphEditor.Models;
 using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Input;
 
 namespace GraphEditor.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public const double NodeSize = 30;
-
     public string MyString => "valuee";
 
     public ViewModelBase CurrentViewModel => this;
 
     public ObservableCollection<Node> Nodes { get; set; } = [];
+
+    public double NodeSize => 30;
 
     public ICommand<Point> AddNodeCommand { get; }
 
--- GraphEditor/Views/MainView.xaml.cs
+++ GraphEditor/Views/MainView.xaml.cs
@@ -2,6 +2,8 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace GraphEditor.Views;
 
@@ -10,6 +12,8 @@ namespace GraphEditor.Views;
 /// </summary>
 public partial class MainView : UserControl
 {
+    private const double NodeSize = 25;
+
     public MainView()
     {
         InitializeComponent();
@@ -20,5 +24,23 @@ public partial class MainView : UserControl
         var vm = DataContext as MainViewModel;
         var clickPosition = e.GetPosition(sender as UIElement);
         vm?.AddNodeCommand.Execute(clickPosition);
+        DrawPoints(vm!.Nodes.Select(p => p.Position));
+    }
+
+    private void DrawPoints(IEnumerable<Point> points)
+    {
+        foreach (var point in points)
+        {
+            Ellipse ellipse = new()
+            {
+                Width = NodeSize,
+                Height = NodeSize,
+                Fill = Brushes.Red
+            };
+
+            Canvas.SetLeft(ellipse, point.X - NodeSize / 2);
+            Canvas.SetTop(ellipse, point.Y - NodeSize / 2);
+            PointsCanvas.Children.Add(ellipse);
+        }
     }
 }
