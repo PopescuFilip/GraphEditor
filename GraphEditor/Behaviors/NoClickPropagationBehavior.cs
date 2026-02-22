4b3a90487bf43d84026ffe127bff723b299e26e4
FilipPopescu <filippopescu18@gmail.com>
2026-02-22 20:53:22 +0200

    Added NoClickPropagationBehavior

---
+++ GraphEditor/Behaviors/NoClickPropagationBehavior.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GraphEditor.Behaviors;
+
+public class NoClickPropagationBehavior : Behavior<UIElement>
+{
+    protected override void OnAttached() =>
+        AssociatedObject.MouseLeftButtonDown += ClickHandler;
+
+    protected override void OnDetaching() =>
+        AssociatedObject.MouseLeftButtonDown -= ClickHandler;
+
+    private void ClickHandler(object _, MouseButtonEventArgs e) => e.Handled = true;
+}
--- GraphEditor/Views/NodeView.xaml.cs
+++ GraphEditor/Views/NodeView.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace GraphEditor.Views
+namespace GraphEditor.Views;
+
+/// <summary>
+/// Interaction logic for NodeView.xaml
+/// </summary>
+public partial class NodeView : UserControl
 {
-    /// <summary>
-    /// Interaction logic for NodeView.xaml
-    /// </summary>
-    public partial class NodeView : UserControl
+    public NodeView()
     {
-        public NodeView()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        Console.WriteLine("dsas");
     }
 }
