10a4c3e99d1e79c6f7fadd0adc2c6d25e6036e60
FilipPopescu <filippopescu18@gmail.com>
2026-02-24 21:38:39 +0200

    Created GenericAlgorithm

---
+++ GraphEditor/Algorithms/GenericAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using GraphEditor.Models;
+
+namespace GraphEditor.Algorithms;
+
+public class GenericAlgorithm
+{
+    public (uint MaxFlux, Graph ResultingGraph) Run(Graph graph) =>
+        Run(graph, graph.Nodes.Min(x => x.Number), graph.Nodes.Max(x => x.Number));
+
+    public (uint MaxFlux, Graph ResultingGraph) Run(Graph graph, int startNode, int endNode)
+    {
+
+        return (0, graph);
+    }
+}
