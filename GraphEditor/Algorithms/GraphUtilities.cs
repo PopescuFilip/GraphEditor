0411cc83b22ebcfcd8c20c75a4bf878fd747bcaa
FilipPopescu <filippopescu18@gmail.com>
2026-03-01 12:52:05 +0200

    Added GraphUtilities

---
+++ GraphEditor/Algorithms/GraphUtilities.cs
@@ -0,0 +1,14 @@
+﻿namespace GraphEditor.Algorithms;
+
+public static class GraphUtilities<T>
+{
+    public static readonly EqualityComparer<KeyValuePair<(int, int), T>> EdgeComparer =
+        EqualityComparer<KeyValuePair<(int, int), T>>.Create(OrderInsensitiveEdgeEquality);
+
+    public static bool OrderInsensitiveEdgeEquality(KeyValuePair<(int, int), T> first, KeyValuePair<(int, int), T> second) =>
+        OrderInsensitiveTupleEquality(first.Key, second.Key);
+
+    private static bool OrderInsensitiveTupleEquality((int, int) first, (int, int) second) =>
+        first.Item1 == second.Item1 && first.Item2 == second.Item2 ||
+        first.Item1 == second.Item2 && first.Item2 == second.Item1;
+}
