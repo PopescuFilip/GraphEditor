e820e676495dc4c262430322f8aeda88fb3c7c93
FilipPopescu <filippopescu18@gmail.com>
2026-03-22 20:38:13 +0200

    Created MinResidualGraphState

---
+++ GraphEditor/Algorithms/Models/MinResidualGraphState.cs
@@ -0,0 +1,21 @@
+﻿using GraphEditor.Models;
+using System.Collections.Immutable;
+
+namespace GraphEditor.Algorithms.Models;
+
+public record MinResidualGraphState(
+    ImmutableDictionary<int, ImmutableArray<int>> AdjacencyList,
+    ImmutableDictionary<(int, int), ResidualEdge> Edges,
+    int MinResidualValue)
+    : GraphState<ResidualEdge>(AdjacencyList, Edges);
+
+public static class MinResidualGraphStateExtensions
+{
+    public static MinResidualGraphState ToMinResidual(this GraphState<ResidualEdge> graphState, int minResidualValue)
+    {
+        var eligibleEdges = graphState.Edges.Values.Where(e => e.ResidualValue >= minResidualValue);
+        var newGraphState = GraphState.CreateRange(eligibleEdges);
+
+        return new MinResidualGraphState(newGraphState.AdjacencyList, newGraphState.Edges, minResidualValue);
+    }
+}
