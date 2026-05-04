5ffce8ecbb61c20f1c0d218bdcc497a7bd36d29b
FilipPopescu <filippopescu18@gmail.com>
2026-05-04 18:13:46 +0300

    Added Fifo preflux algorithm

---
+++ GraphEditor/Algorithms/Tagging/FifoPrefluxAlgorithm.cs
@@ -0,0 +1,70 @@
+﻿using GraphEditor.Algorithms.Models;
+using GraphEditor.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GraphEditor.Algorithms.Tagging;
+
+public class FifoPrefluxAlgorithm
+{
+    public (int MaxFlow, Graph ResultingGraph) Run(Graph graph, Action<Graph<ResidualEdge>> onNewResidualGraph) =>
+        Run(graph, graph.Nodes.Min(x => x.Number), graph.Nodes.Max(x => x.Number), 0, onNewResidualGraph);
+
+    public (int MaxFlow, Graph ResultingGraph) Run(Graph graph, int startNode, int endNode, int initialFlow,
+        Action<Graph<ResidualEdge>> onNewResidualGraph)
+    {
+        var maxFlow = initialFlow;
+        var graphState = GraphState.CreateRange(graph.Edges);
+        var residualGraph = graphState.ToResidual();
+        onNewResidualGraph(new Graph<ResidualEdge>(graph.Nodes, [.. residualGraph.Edges.Values]));
+
+        var tags = residualGraph.GetDistanceTags(endNode);
+        while (TryFindWayToEndNode(residualGraph, startNode, endNode, tags, graph.Nodes.Length, out var wayToEndNode))
+        {
+            var maxWayFlow = residualGraph.GetMinResidualValue(wayToEndNode);
+            maxFlow += maxWayFlow;
+            graphState = graphState.AddFlow(wayToEndNode, maxWayFlow);
+            residualGraph = graphState.ToResidual();
+            onNewResidualGraph(new Graph<ResidualEdge>(graph.Nodes, [.. residualGraph.Edges.Values]));
+        }
+
+        return (maxFlow, graph with { Edges = [.. graphState.GetEdges()] });
+    }
+
+    private bool TryFindWayToEndNode(GraphState<ResidualEdge> graphState, int startNode, int endNode,
+        Dictionary<int, int> tags,
+        int nodesCount,
+        [NotNullWhen(true)] out Way? wayToEndNode)
+    {
+        wayToEndNode = null;
+
+        var parents = new Dictionary<int, int>();
+        var currentNode = startNode;
+
+        while (tags[startNode] < nodesCount)
+        {
+            var admissibleNodes = graphState.GetAdmissibleNodes(currentNode, tags).ToList();
+            if (admissibleNodes.Count == 0)
+            {
+                var minDistance = graphState.AdjacencyList[currentNode].Select(node => tags[node]).Min();
+                tags[currentNode] = minDistance + 1;
+
+                if (currentNode != startNode)
+                    currentNode = parents[currentNode];
+                continue;
+            }
+
+            admissibleNodes.Shuffle();
+            var admissibleNode = admissibleNodes.First();
+            parents[admissibleNode] = currentNode;
+            currentNode = admissibleNode;
+
+            if (currentNode == endNode)
+            {
+                wayToEndNode = parents.ToWay(endNode);
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
