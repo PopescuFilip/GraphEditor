f99ed7cfed9c4f090e4f1b4434cf934742fb9042
FilipPopescu <filippopescu18@gmail.com>
2026-05-04 19:05:18 +0300

    Added ExcessEdge

--- GraphEditor/Models/Edge.cs
+++ GraphEditor/Models/Edge.cs
@@ -5,3 +5,5 @@ public record SimpleEdge(int StartNode, int EndNode);
 public record ResidualEdge(int StartNode, int EndNode, int ResidualValue) : SimpleEdge(StartNode, EndNode);
 
 public record FlowEdge(int StartNode, int EndNode, int Flow, int Capacity) : SimpleEdge(StartNode, EndNode);
+
+public record ExcessEdge(int StartNode, int EndNode, int Flow, int Capacity, int Excess) : FlowEdge(StartNode, EndNode, Flow, Capacity);
